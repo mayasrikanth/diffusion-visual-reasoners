Fixed clip-r precision calculation to calculate clip-r score for the R=50% of total corpus

comp-t2i-dataset/clip_trainer.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 import datasets
 from torch.utils.data import DataLoader
@@ -6,9 +7,11 @@ from models.clip_r_precision import CLIPRPrecision
 import torch
 from clip import clip
 
-TRAIN_DATA_DIR = '/home/jasonlin/repos/datasets/t2i_benchmark/winoground/hf/winoground-hf-arrow-label'
+#TRAIN_DATA_DIR = '/home/jasonlin/repos/datasets/t2i_benchmark/winoground/hf/winoground-hf-arrow-label'
+TRAIN_DATA_DIR = '/home/mayashar/Desktop/gqa_winogrand_val_clipR'
 TRAIN_PARQUET = '/home/jasonlin/repos/datasets/t2i_benchmark/winoground/hf/winoground-hf-train.parquet'
-CLIP_PATH = '/home/jasonlin/repos/comp-t2i-dataset/clip_weights/winoground/winoground_shape.pt'
+#CLIP_PATH = '/home/jasonlin/repos/comp-t2i-dataset/clip_weights/winoground/winoground_shape.pt'
+CLIP_PATH = '/home/mayashar/Desktop/C-CUB/C_CUB_shape.pt'
 
 def train(train_data):
     # load the model
@@ -25,20 +28,29 @@ def train(train_data):
 wandb_logger = WandbLogger(name='winoground_clip_ft_debug',project='diffusion-reasoner')
 
 # preprocess data
-hf_dataset = datasets.load_from_disk(TRAIN_DATA_DIR)
 def transforms(examples):
+    print("EXAMPLES")
+    print(examples)
     # reshape images to 224, 224 as clip expects image HW = 224, token length = 77 
     examples["image"] = [image.convert("RGB").resize((224,224)) for image in examples["image"]]
     examples["label"] = clip.tokenize(examples["label"])
     return examples
+
+hf_dataset = datasets.load_from_disk(TRAIN_DATA_DIR).with_format("torch")
+print("TYPE OF DATASET: ", type(hf_dataset))
+print("DATA: ", hf_dataset[0])
 hf_dataset = hf_dataset.map(transforms, batched=True)
+print("ANOTHER TYPING: ", type(hf_dataset))
 # import pdb; pdb.set_trace()
-wino_train_loader = DataLoader(hf_dataset['train'].with_format("torch"), batch_size=512)
+# wino_train_loader = DataLoader(hf_dataset['train'].with_format("torch"), batch_size=512)
+
+wino_train_loader = DataLoader(hf_dataset.with_format("torch"), batch_size=512)
 
 # dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
 # train_loader = utils.data.DataLoader(dataset)
-trainer = pl.Trainer(max_epochs=200, accelerator="gpu", auto_lr_find=True, 
-    log_every_n_steps=5, logger=wandb_logger)   # limit_train_batches=1, logger=wandb_logger
+#Trainer(callbacks=[CheckpointEveryNSteps()])
+trainer = pl.Trainer(max_epochs=15, accelerator="gpu", auto_lr_find=True, 
+    log_every_n_steps=5, logger=wandb_logger, callbacks=[ModelCheckpoint(dirpath='./',every_n_epochs=2)])   # limit_train_batches=1, logger=wandb_logger
 
 # import pdb; pdb.set_trace()
 train(wino_train_loader)

comp-t2i-dataset/compute_r_precision.py
@@ -23,6 +23,26 @@ def r_precision(candidates, num_chunks=10):
 
     return np.average(chunks), np.std(chunks)
 
+def r_precision2(candidates, num_chunks=10):
+    predictions = []
+    for cand in candidates:
+        print(cand["prediction"])
+        if 0 in cand["prediction"]: #cand["prediction"] == 0:
+            predictions.append(1)
+        else:
+            predictions.append(0)
+
+    num_preds = len(predictions)
+    chunk_size = int(num_preds / num_chunks)
+
+    predictions = np.array(predictions)
+    np.random.shuffle(predictions)
+
+    chunks = np.zeros(num_chunks)
+    for i in range(num_chunks):
+        chunks[i] = np.average(predictions[i * chunk_size : (i + 1) * chunk_size])
+
+    return np.average(chunks), np.std(chunks)
 
 
 if __name__ == "__main__":
@@ -36,12 +56,18 @@ if __name__ == "__main__":
                         help="Type of composition [color, shape].")
     parser.add_argument("--split", default="", type=str,
                         help="Test split to use [test_seen, test_unseen, test_swapped].")
-    parser.add_argument("--pred_path", default="", type=str,
+    # eval_data/glidelaion_noFT_drawbench_clip_preds.pkl
+    # eval_data/glidelaion_FTwino_drawbench_clip_preds.pkl
+    # eval_data/glideliaon_noFT_winoground_clip_preds.pkl 
+    parser.add_argument("--pred_path", default="eval_data/glidelaion_FTwino_winoground_clip_preds.pkl", type=str,
                         help="Path to the generated image results.")
     args = parser.parse_args()
 
-    anno_data_path = f"./data/{args.dataset}/{args.comp_type}/data.pkl"
-    split_path = f"./data/{args.dataset}/{args.comp_type}/split.pkl"
+    # anno_data_path = f"./data/{args.dataset}/{args.comp_type}/data.pkl"
+    # split_path = f"./data/{args.dataset}/{args.comp_type}/split.pkl"
+    anno_data_path = '/home/mayashar/Desktop/diffusion-visual-reasoners/comp-t2i-dataset/eval_data/data.pkl' 
+    
+    split_path = '/home/mayashar/Desktop/diffusion-visual-reasoners/comp-t2i-dataset/eval_data/split.pkl'
 
     # separate result files for each split
     with open(args.pred_path, "rb") as f:
@@ -60,13 +86,14 @@ if __name__ == "__main__":
         candidates = []
         for entry in result:
             img_id, cap_id, gen_img_path, r_precision_prediction = entry
+            print(r_precision_prediction)
             candidates.append({
                 "prediction": r_precision_prediction,
                 "img_path": gen_img_path,
                 "text": anno_data[img_id][cap_id]["text"]
             })
 
-    R_mean, R_std = r_precision(candidates)
+    R_mean, R_std = r_precision2(candidates)
 
     print("R Precision score: ")
     print(f"\t {R_mean * 100:.2f} +- {R_std * 100:.2f}")

comp-t2i-dataset/make_clip_prediction.py
@@ -2,14 +2,15 @@ import os
 import pickle
 import csv
 import torch
+import numpy as np 
 
-from datasets import WinogroundDataset, CCUBDataset, CFlowersDataset
+from datasets_t2i import WinogroundDataset, CCUBDataset, CFlowersDataset
 from models.clip_r_precision import CLIPRPrecision
 from clip import clip
 from tqdm import tqdm
 
 from PIL import Image
-
+# Assumes you've run prep_eval_data with the relevant data 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -33,12 +34,17 @@ if __name__ == "__main__":
                         help="path to output (this script outputs a pickle file")
     parser.add_argument("--csv_out", default="clip_r_precision_results.csv", type=str,
                         help="path to output (this script outputs a CSV file")
+    parser.add_argument("--clipr_scores_csv", default="all_clipR_winoground_glidelaion_noFT.csv", type=str,
+                        help="path to output (this script outputs a CSV file")
 
     args = parser.parse_args()
 
     # separate result files for each split
     with open(args.pred_path, "rb") as f:
         result = pickle.load(f)
+
+    # print("RESULT: ", result)
+    print("LENGHT OF RESULT INITIALLY: ", len(result))
 
     torch.cuda.set_device(args.gpu)
     torch.backends.cudnn.benchmark = True
@@ -58,7 +64,9 @@ if __name__ == "__main__":
 
     # dataset creation
     # data_dir = "./data"
-    data_dir = "/home/jasonlin/repos/datasets/t2i_benchmark"
+    #data_dir = "/home/jasonlin/repos/datasets/t2i_benchmark"
+    data_dir = 'someshit'
+    # data_dir should contain data.pkl and split.pkl 
     if args.dataset == "C-CUB":
         images_txt_path = os.path.join(data_dir, "C-CUB", "images.txt")
         bbox_txt_path = os.path.join(data_dir, "C-CUB", "bounding_boxes.txt")
@@ -117,7 +125,8 @@ if __name__ == "__main__":
             continue
         mismatched_captions = dataset.get_mismatched_caption(img_id).cuda()
         all_texts = torch.cat([text_conditioned, mismatched_captions], 0)
-
+        print("SHAPE OF all_texts: ", all_texts.shape)
+        R_limit = int(0.50 * len(all_texts))
         with torch.no_grad():
             image_features, text_features = model(image, all_texts)
 
@@ -126,20 +135,34 @@ if __name__ == "__main__":
 
             logit_scale = model.logit_scale.exp()
             logits_per_image = logit_scale * image_features @ text_features.t()
-
-            clip_prediction = torch.argsort(logits_per_image, dim=1, descending=True)[0, 0].item()
+            
+            #clip_prediction = torch.argsort(logits_per_image, dim=1, descending=True)[0, 0].item()
+            clip_prediction = torch.argsort(logits_per_image, dim=1, descending=True)[:, :R_limit] #.item()
+            print("CLIP PREDICTION SHAPE: ", clip_prediction.shape)
+            print(clip_prediction)
 
         new_entry = (img_id, cap_id, gen_img_path, clip_prediction)
         clip_result.append(new_entry)
 
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     with open(args.out_path, 'wb') as f:
         pickle.dump(clip_result, f)
     
-    with open("/home/jasonlin/repos/datasets/t2i_benchmark/winoground/shape/data.pkl", "rb") as wino_df_file:
-        ex_data = pickle.load(wino_df_file)
-        with open(args.csv_out, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-            for (img_id, cap_id, gen_img_path, r_precision_prediction) in clip_result:
-                csv_writer.writerow([img_id, ex_data[img_id][0]["text"], r_precision_prediction])
-        # '/home/jasonlin/repos/datasets/t2i_benchmark/winoground/wino_gt_clipr.csv'
+    # Compute average clip-R score 
+    # nums = [num for (path, x, f, num) in clip_result]
+    # print("AVERAGE CLIP-R SCORE: ", np.mean(nums))
+
+    # with open("/home/mayashar/Desktop/diffusion-visual-reasoners/comp-t2i-dataset/eval_data/data.pkl", "rb") as wino_df_file:
+    #     ex_data = pickle.load(wino_df_file)
+    #     with open(args.csv_out, 'w', newline='') as csvfile:
+    #         csv_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    #         for (img_id, cap_id, gen_img_path, r_precision_prediction) in clip_result:
+    #             csv_writer.writerow([img_id, ex_data[img_id][0]["text"], r_precision_prediction])
+    #     # '/home/jasonlin/repos/datasets/t2i_benchmark/winoground/wino_gt_clipr.csv'
+
+    # print("Output scores file: ", args.clipr_scores_csv)
+
+    # with open(args.clipr_scores_csv, 'w', newline='') as csvfile:
+    #         csv_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    #         for (img_id, cap_id, gen_img_path, r_precision_prediction) in clip_result:
+    #             csv_writer.writerow([img_id, r_precision_prediction])